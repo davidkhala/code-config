04188a238aef7bbc06a794e66af8326cb8f7bc40
david <david-khala@hotmail.com>
2019-08-27 10:03:15 +0800

    BASH_ENV persist

--- .circleci/helper.sh
+++ .circleci/helper.sh
@@ -4,10 +4,9 @@ nvm8() {
     export NVM_DIR="/opt/circleci/.nvm"
 		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-    echo BASH_ENV = $BASH_ENV
     echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
     echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
     nvm install 8
-    node --version
+    which node
 }
 $1
