62ffe91d1e237aab4a9305f4e8b57f5ef7c6c150
david <david-khala@hotmail.com>
2019-08-27 10:09:35 +0800

    try not to use BASH_ENV

--- .circleci/helper.sh
+++ .circleci/helper.sh
@@ -4,8 +4,8 @@ nvm8() {
     export NVM_DIR="/opt/circleci/.nvm"
 		[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-    echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
-    echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
+#    echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
+#    echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
     nvm install 8
 		nvm alias default v8
 
