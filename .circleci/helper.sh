3f8f77d8a66f93463b429bf3dc35bd343cd07120
david <david-khala@hotmail.com>
2019-08-27 10:08:13 +0800

    [test]add alias

--- .circleci/helper.sh
+++ .circleci/helper.sh
@@ -7,6 +7,8 @@ nvm8() {
     echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
     echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
     nvm install 8
+		nvm alias default v8
+
     which node
 }
 $1
